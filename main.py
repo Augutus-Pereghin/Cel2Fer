5a1493335d998d49ca445051a52ced15327b533e
AugustusP <agp5191@psu.edu>
2020-08-27 14:14:43 +0000

    different spelling change

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-tempCel = float(input("Enter temperature in celcius: "))
+tempCel = float(input("Enter temperature in celsius: "))
 tempFahr = tempCel * 9/5 + 32
 print(str(tempCel) + "° in Celsius is equivalent to " + str(tempFahr) + "° Fahrenheit.")
