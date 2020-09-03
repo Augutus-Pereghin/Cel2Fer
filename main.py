e997d3d11b2cd3d4d523d8edb0d1ee61c787a116
AugustusP <agp5191@psu.edu>
2020-09-03 14:42:38 +0000

    added reverse functionality

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-tempCel = float(input("Enter temperature in celsius: "))
-tempFahr = tempCel * 9/5 + 32
-print(str(tempCel) + "° in Celsius is equivalent to " + str(tempFahr) + "° Fahrenheit.")
+# Author: Augustus Perseghin
+# Collaborator: ZiHao Xu
+# Collaborator: Chenyin Zhang
+
+tempIn = float(input("Enter temperature: "))
+tempInType = input("Enter unit in F/f or C/c: ")
+tempOut = 0
+if tempInType == 'c' or tempInType == 'C':
+  tempOut = tempIn * 9/5 + 32
+  print(f"{tempIn}° in Celsius is equivalent to {tempOut}° Fahrenheit.")
+elif tempInType == 'f' or tempInType == 'F':
+  tempOut = (tempIn - 32) * 5/9
+  print(f"{tempIn}° in Fahrenheit is equivalent to {tempOut}° Celsius.")
+else:
+  print(f"invalid unit({tempInType})")
